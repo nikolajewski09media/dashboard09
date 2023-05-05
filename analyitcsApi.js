24cd9a7c5ea3f5341fd2834a84354bc7c7c589fb
nikolajewski09media <herby.nik@gmail.com>
2023-05-05 09:21:21 +0200

    inital commit

---
+++ analyitcsApi.js
@@ -0,0 +1,42 @@
+/**
+ * TODO(developer): Uncomment this variable and replace with your
+ *   Google Analytics 4 property ID before running the sample.
+ */
+const propertyId = "355361722";
+
+// Imports the Google Analytics Data API client library.
+import { BetaAnalyticsDataClient } from "@google-analytics/data";
+
+// Using a default constructor instructs the client to use the credentials
+// specified in GOOGLE_APPLICATION_CREDENTIALS environment variable.
+const analyticsDataClient = new BetaAnalyticsDataClient();
+
+// Runs a simple report.
+export default async function runReport() {
+  const [response] = await analyticsDataClient.runReport({
+    property: `properties/${propertyId}`,
+    dateRanges: [
+      {
+        startDate: "2020-03-31",
+        endDate: "today",
+      },
+    ],
+    dimensions: [
+      {
+        name: "sessionDefaultChannelGroup",
+      },
+    ],
+    metrics: [
+      {
+        name: "sessions",
+      },
+    ],
+  });
+
+  console.log("Report result for " + propertyId + ":");
+  response.rows.forEach((row) => {
+    console.log(row.dimensionValues[0].value, row.metricValues[0].value);
+  });
+}
+
+runReport();
