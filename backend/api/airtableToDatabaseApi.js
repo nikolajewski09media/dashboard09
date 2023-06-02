1b9ebe1e1203b8b49137ed6613e0d39e90210ca3
nikolajewski09media <nikolajewsk@09media.de>
2023-06-02 16:20:12 +0200

    properties datenbank geupdated

--- backend/api/airtableToDatabaseApi.js
+++ backend/api/airtableToDatabaseApi.js
@@ -1,52 +1,58 @@
-import Airtable from 'airtable';
+import Airtable from "airtable";
 
-import * as dotenv from 'dotenv'
-dotenv.config()
+import * as dotenv from "dotenv";
+dotenv.config();
 
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 
-import { join, dirname } from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-import { Low } from 'lowdb'
-import { JSONFile } from 'lowdb/node'
-
+import { Low } from "lowdb";
+import { JSONFile } from "lowdb/node";
 
 export default async function updateDB() {
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const file = join(__dirname, "..", "db", "propertiesV4.DB.json");
 
-    const __dirname = dirname(fileURLToPath(import.meta.url))
-    const file = join(__dirname, '..', 'db', 'propertiesV4.DB.json')
+    const adapter = new JSONFile(file);
+    const defaultData = { properties: [] };
+    const db = new Low(adapter, defaultData);
 
-
-    const adapter = new JSONFile(file)
-    const defaultData = { properties: [] }
-    const db = new Low(adapter, defaultData)
-
-
-    const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('appM88vCbapvrcTCi');
-    await db.read()
-
+    const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+        "appM88vCbapvrcTCi"
+    );
+    await db.read();
 
     db.data.properties = [];
-    await db.write()
+    await db.write();
 
+    base("Domains")
+        .select({
+            // maxRecords: 3,
+            view: "Grid view",
+        })
+        .eachPage(
+            function page(records, fetchNextPage) {
+                records.forEach(async function (record) {
+                    // Hier passiert das schreiben in die porperties-Datenbank
+                    const propApiObj = {
+                        label: record.get("Domain"),
+                        id: record.get("Porperty-ID v4 (from Porperty-ID v4)")[0],
+                        gewerk: record.get("Gewerke (from Gewerke)")[0],
+                    };
+                    if (record.get("Untergewerke (from Untergewerke)")) propApiObj.untergewerk = record.get("Untergewerke (from Untergewerke)");
+                    db.data.properties.push(propApiObj);
+                    await db.write();
+                });
 
-    base('Domains').select({
-        // maxRecords: 3,
-        view: "Grid view"
-    }).eachPage(function page(records, fetchNextPage) {
-
-
-        records.forEach(async function (record) {
-            // Hier passiert das schreiben in die porperties-Datenbank
-            db.data.properties.push({ label: record.get('Domain'), id: record.get('Porperty-ID v4 (from Porperty-ID v4)')[0] })
-            await db.write()
-        });
-
-        fetchNextPage();
-
-    }, function done(err) {
-        if (err) { console.error(err); return; }
-    });
+                fetchNextPage();
+            },
+            function done(err) {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+            }
+        );
 }
 
 // updateDB()
