07b9dab4c5e23e7bcd717b155a5dbf563220433f
nikolajewski09media <nikolajewsk@09media.de>
2023-06-08 18:04:25 +0200

    backendend Bugfix

--- backend/controller/analycticsApi.controller.js
+++ backend/controller/analycticsApi.controller.js
@@ -1,55 +1,57 @@
 import runReport from "../api/analyticsApi.js";
 import properties from "../model/propertiesV4.model.js";
-import { getStatistic, setStatistic, getOneStatistic } from "../model/statistic.model.js";
-
-
+import {
+  getStatistic,
+  setStatistic,
+  getOneStatistic,
+} from "../model/statistic.model.js";
 
 export async function getOneReport(req, res) {
-    const { propertyId } = req.params;
-    // const reportData = await runReport(propertyId, reportDate, reportDate);
-    const reportData = getOneStatistic(propertyId)
-    res.json(reportData);
+  const { propertyId } = req.params;
+  // const reportData = await runReport(propertyId, reportDate, reportDate);
+  const reportData = getOneStatistic(propertyId);
+  res.json(reportData);
 }
 
 export async function setAllReport(req, res) {
-    await runAllReport();
-    res.send("Statistik wurde erfolgreich aktualisiert!");
+  await runAllReport();
+  res.send("Statistik wurde erfolgreich aktualisiert!");
 }
 
 export function getAllStatistic(req, res) {
-    const stats = getStatistic();
-    res.json(stats);
+  const stats = getStatistic();
+  res.json(stats);
 }
 
 export async function runAllReport() {
-    const yesterdate = new Date();
-    yesterdate.setDate(yesterdate.getDate() - 1);
-    const [reportDate] = yesterdate.toISOString().split("T");
+  const yesterdate = new Date();
+  yesterdate.setDate(yesterdate.getDate() - 1);
+  const [reportDate] = yesterdate.toISOString().split("T");
 
-    const reportData = [];
-    // console.log('Fetch hat begonnen. Kann ca. 1-2 Minuten dauern!')
-    for (const property of properties) {
-        const singleReportData = await runReport(
-            property.id,
-            reportDate,
-            reportDate
-        );
+  const reportData = [];
+  // console.log('Fetch hat begonnen. Kann ca. 1-2 Minuten dauern!')
+  for (const property of properties) {
+    const singleReportData = await runReport(
+      property.id,
+      reportDate,
+      reportDate
+    );
 
-        if (Object.keys(singleReportData).length !== 4) {
-            singleReportData["Paid Search"] = singleReportData["Paid Search"] || "0"
-            singleReportData["Organic Search"] = singleReportData["Organic Search"] || "0"
-            singleReportData.Direct = singleReportData.Direct || "0"
-            singleReportData.Unassigned = singleReportData.Unassigned || "0"
-        }
-
-        reportData.push({
-            label: property.label,
-            id: property.id,
-            reports: [{ [reportDate]: { ...singleReportData } }],
-        });
-        // console.log({ [reportDate]: { ...singleReportData } })
-
+    if (Object.keys(singleReportData).length !== 4) {
+      singleReportData["Paid Search"] = singleReportData["Paid Search"] || "0";
+      singleReportData["Organic Search"] =
+        singleReportData["Organic Search"] || "0";
+      singleReportData.Direct = singleReportData.Direct || "0";
+      singleReportData.Unassigned = singleReportData.Unassigned || "0";
     }
-    setStatistic(reportData);
-    console.log('Daten wurden erfolgreich von GA gefetched für', reportDate);
+
+    reportData.push({
+      label: property.label,
+      id: property.id,
+      reports: [{ [reportDate]: { ...singleReportData } }],
+    });
+    // console.log({ [reportDate]: { ...singleReportData } })
+  }
+  setStatistic(reportData);
+  console.log("Daten wurden erfolgreich von GA gefetched für", reportDate);
 }
--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,38 @@
-import express from 'express'
-import cron from 'node-cron'
+import express from "express";
+import cron from "node-cron";
 
-import { getAllStatistic, getOneReport, runAllReport, setAllReport } from './controller/analycticsApi.controller.js';
-import { refreshDB } from './controller/airtableToDatabaseApi.controller.js';
-
-
+import {
+  getAllStatistic,
+  getOneReport,
+  runAllReport,
+  setAllReport,
+} from "./controller/analycticsApi.controller.js";
+import { refreshDB } from "./controller/airtableToDatabaseApi.controller.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.get("/", (req, res) => {
-    return res.send("Hello World!")
-})
-
+  return res.send("Hello World!");
+});
 
 app.get("/api/property/:propertyId", getOneReport);
 
 app.get("/api/allProperties", getAllStatistic);
 
-
-
-
 app.listen(PORT, async () => {
-    console.log(`Server is listening on port ${PORT}`);
-    console.log(`Serves is reachable at http://localhost:${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
+  console.log(`Serves is reachable at http://localhost:${PORT}`);
+  // for (let i = 3; i < 34; i++) {
+  //     await runAllReport(i)
+  // }
+
+  // Dieser Befehl führt den API-Fetch von Airtaible jede sechs Stunden aus
+  cron.schedule("0 0 4 * * *", () => {
     refreshDB();
-    // for (let i = 3; i < 34; i++) {
-    //     await runAllReport(i)
-    // }
-
-    // Dieser Befehl fürt den API-Fetch von Airtaible jede sechs Stunden aus
-    cron.schedule('0 0 */6 * * *', () => {
-        refreshDB();
-    });
-    // Dieser Befehl fürt den API-Fetch von GA jeden Tag um 01:00 Uhr Nachts aus
-    cron.schedule('0 0 1 * * *', async () => {
-        await runAllReport()
-
-    });
-
-})
+  });
+  // Dieser Befehl führt den API-Fetch von GA jeden Tag um 01:00 Uhr Nachts aus
+  cron.schedule("0 0 5 * * *", async () => {
+    await runAllReport();
+  });
+});
