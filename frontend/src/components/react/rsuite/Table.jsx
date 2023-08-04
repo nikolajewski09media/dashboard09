dbefa60e2681ef6e803a7a79e0e109f3db85de9a
nikolajewski09media <nikolajewsk@09media.de>
2023-08-04 15:58:11 +0200

    new table component

---
+++ frontend/src/components/react/rsuite/Table.jsx
@@ -0,0 +1,102 @@
+import { Table, Pagination } from "rsuite";
+import React from "react";
+const { Column, HeaderCell, Cell } = Table;
+import { useStore } from "@nanostores/react";
+import { dates, $fetchedData, getDataInRange } from "../../../utils/dataStore";
+
+export default function CustomTable({ initalData }) {
+  const [limit, setLimit] = React.useState(10);
+  const [page, setPage] = React.useState(1);
+  const [defaultData, setDefaultData] = React.useState(initalData);
+  const [sortColumn, setSortColumn] = React.useState("clicks");
+  const [sortType, setSortType] = React.useState("desc");
+  const [loading, setLoading] = React.useState(false);
+
+  const handleSortColumn = (sortColumn, sortType) => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setSortColumn(sortColumn);
+      setSortType(sortType);
+    }, 500);
+  };
+
+  const handleChangeLimit = (dataKey) => {
+    setPage(1);
+    setLimit(dataKey);
+  };
+
+  const data = defaultData.filter((v, i) => {
+    const start = limit * (page - 1);
+    const end = start + limit;
+    return i >= start && i < end;
+  });
+  const $dates = useStore(dates);
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const data = await $fetchedData.get();
+      const rowsArr = await getDataInRange(data, $dates);
+      if (sortColumn && sortType) {
+        rowsArr.sort((a, b) => {
+          let x = a[sortColumn];
+          let y = b[sortColumn];
+          if (typeof x === "string") {
+            x = x.charCodeAt();
+          }
+          if (typeof y === "string") {
+            y = y.charCodeAt();
+          }
+          if (sortType === "desc") {
+            return y - x;
+          } else {
+            return x - y;
+          }
+        });
+      }
+      setDefaultData(rowsArr);
+    }
+    fetchData();
+  }, [$dates, sortColumn, sortType]);
+
+  return (
+    <div>
+      <Table
+        height={500}
+        data={data}
+        sortColumn={sortColumn}
+        sortType={sortType}
+        onSortColumn={handleSortColumn}
+        loading={loading}
+      >
+        <Column align="left" flexGrow={1}>
+          <HeaderCell>Domains</HeaderCell>
+          <Cell dataKey="name" />
+        </Column>
+
+        <Column flexGrow={2} align="right" sortable>
+          <HeaderCell> Gesamte Sessions</HeaderCell>
+          <Cell dataKey="clicks" />
+        </Column>
+      </Table>
+      <div style={{ padding: 20 }}>
+        <Pagination
+          prev
+          next
+          first
+          last
+          boundaryLinks
+          maxButtons={5}
+          size="md"
+          layout={["total", "-", "limit", "|", "pager"]}
+          total={defaultData.length}
+          limitOptions={[10, 30, 50]}
+          limit={limit}
+          activePage={page}
+          onChangePage={setPage}
+          onChangeLimit={handleChangeLimit}
+        />
+      </div>
+    </div>
+  );
+}
