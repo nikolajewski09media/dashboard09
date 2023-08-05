88dd4e5c4fa3f20e15b8064c3c7b4c04765b2cdd
nikolajewski09media <nikolajewsk@09media.de>
2023-08-05 15:02:39 +0200

    netlyfy

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
+import cloudflare from "@astrojs/cloudflare";
 import vercel from "@astrojs/vercel/serverless";
+import node from "@astrojs/node";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: vercel(),
+  adapter: netlify()
 });
