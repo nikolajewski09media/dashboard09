fcf553a8f8b731546665f3729a2f625ed00b3ce6
nikolajewski09media <nikolajewsk@09media.de>
2023-08-09 10:54:04 +0200

    setup for login

--- frontend_shadcn/astro.config.mjs
+++ frontend_shadcn/astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  })],
+  output: "server",
+  adapter: cloudflare()
 });
