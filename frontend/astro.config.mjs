7b3533be04382d63a86decac1523f451606811b3
nikolajewski09media <nikolajewsk@09media.de>
2023-08-09 11:35:55 +0200

    try to deploy on vercel

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import cloudflare from "@astrojs/cloudflare";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,5 @@ export default defineConfig({
     applyBaseStyles: false
   })],
   output: "server",
-  adapter: cloudflare()
+  adapter: vercel()
 });
